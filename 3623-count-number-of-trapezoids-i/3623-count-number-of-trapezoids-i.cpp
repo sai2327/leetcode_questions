3768083beb3d38bf7c3a854f0e6e852e191b55d7
sai2327 <saitejak2327@gmail.com>
2025-12-02 11:36:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        int mod=1e9+7;
+        unordered_map<int ,int>mp;
+        for(auto i:points) mp[i[1]]++;
+        long long sum=0,ans=0;
+        for(auto i:mp){
+            long long ncr=(i.second)*(i.second-1)/2;
+            ans+=sum*ncr;
+            ans%=mod;
+            sum+=ncr;
+            sum%=mod;
+        }
+        return ans;
+    }
+};
