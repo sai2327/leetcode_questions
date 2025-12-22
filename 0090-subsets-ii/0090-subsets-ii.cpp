4655164949a0ec3885dd10283f0d7faa4229ea67
sai2327 <saitejak2327@gmail.com>
2025-12-22 11:24:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    set<vector<int>>res;
+    vector<int>ans;
+    void dfs(vector<int>&nums,int i){
+        if(i>=nums.size()){
+            res.insert(ans);
+            return;
+        }
+        ans.push_back(nums[i]);
+        dfs(nums,i+1);
+        ans.pop_back();
+        dfs(nums,i+1);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        dfs(nums,0);
+        vector<vector<int>>result(res.begin(),res.end());
+        return result;
+    }
+};
