c4b26f39bdc7f41e6657e219c366300d47823b7d
sai2327 <saitejak2327@gmail.com>
2025-12-22 19:09:32 +0530

    Time: 1 ms (58.24%), Space: 23 MB (28.16%) - LeetHub

---
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& f, vector<vector<int>>& s) {
+        vector<vector<int>>res;
+        int i=0,j=0;
+        while(i<f.size() and j<s.size()){
+            int l=max(f[i][0],s[j][0]);
+            int u=min(f[i][1],s[j][1]);
+            if(l<=u) res.push_back({l,u});
+            if(f[i][1]<s[j][1]) i++;
+            else j++;
+        }
+        return res;
+    }
+};
