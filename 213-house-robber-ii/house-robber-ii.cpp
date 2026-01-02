916b9e96778112e6b0d69eca7cdd8c6ab4341c07
sai2327 <saitejak2327@gmail.com>
2026-01-02 19:04:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (87.47%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int spirit(vector<int>&nums,int start,int end){
+        int prev1=0,prev2=0;
+        for(int i=start;i<=end;i++){
+            int temp=max(prev1,prev2+nums[i]);
+            prev2=prev1;
+            prev1=temp;
+        }
+        return prev1;
+    }
+    int rob(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        int case1=spirit(nums,0,nums.size()-2);
+        int case2=spirit(nums,1,nums.size()-1);
+        return max(case1,case2);
+    }
+};
