0454f1bfb85c41efd19bc55a94bad101df4d9b2f
sai2327 <saitejak2327@gmail.com>
2026-01-21 14:56:30 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (83.50%) - LeetSync

--- 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
+++ 3611-construct-the-minimum-bitwise-array-ii/construct-the-minimum-bitwise-array-ii.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>res;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==2)res.push_back(-1);
-            else res.push_back(nums[i] - ((nums[i]+1) & -(nums[i]+1))/2);
+            else res.push_back(nums[i] - (((nums[i]+1) & -(nums[i]+1))>>1));
         }
         return res;
     }
