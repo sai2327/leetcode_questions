dd6ab2d5297e6305e74090f532b56ad837ea3de1
KONDAPALLI SAI TEJA <saitejak2327@gmail.com>
2026-02-24 19:51:52 +0530

    Time: 39 ms (24.59%) | Memory: 21.4 MB (24.86%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size(),-1);
+        vector<int>dp(nums.size(),1e9);
         dp[nums.size()-1]=0;
         for(int i=nums.size()-2;i>=0;i--){
-            int mini=1e9;
             if(nums[i]+i>=nums.size()){
                 dp[i]=1;
                 continue;
             }
+            int mini=1e9;
             for(int j=i+1;j<nums.size() and j<=i+nums[i];j++){
                 mini=min(dp[j],mini);
             }
