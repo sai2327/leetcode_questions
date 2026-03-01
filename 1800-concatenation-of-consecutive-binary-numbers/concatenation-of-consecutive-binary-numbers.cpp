0707484b0a68cb4ea38221ef8cf7bb992b5683ff
KONDAPALLI SAI TEJA <saitejak2327@gmail.com>
2026-03-01 08:09:32 +0530

    Time: 28 ms (71.33%) | Memory: 8.1 MB (41.33%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        int mod=1e9+7;
+        long long res=0;
+        int bits=0;
+        for(int i=1;i<=n;i++){
+            if((i&(i-1))==0)bits++;
+            res=((res<<bits)|i)%mod;
+        }
+        return res;
+    }
+};
