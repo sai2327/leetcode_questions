4cb4171d8ad5ce5a4d20afd7b95ef58033e93382
KONDAPALLI SAI TEJA <saitejak2327@gmail.com>
2026-04-24 14:55:48 +0530

    Time: 7 ms (19.01%) | Memory: 28.7 MB (56.27%) - LeetSync

---
+++ 4252-first-unique-even-element/first-unique-even-element.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int firstUniqueEven(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i:nums)mp[i]++;
+        for(int i:nums){
+            if(i%2==0 and mp[i]==1) return i;
+        }
+        return -1;
+    }
+};
