1201bcfa7e99a8efeee96ce777127dd0bf93e166
KONDAPALLI SAI TEJA <saitejak2327@gmail.com>
2026-05-17 15:52:17 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.37%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0,end=nums.size()-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>=nums[start]){
+                if(target>=nums[start] and nums[mid]>target) end=mid-1;
+                else start=mid+1;
+            }
+            else{
+                if(target>nums[mid] and nums[end]>=target) start=mid+1;
+                else end=mid-1;
+            }
+        }
+        return -1;
+    }
+};
