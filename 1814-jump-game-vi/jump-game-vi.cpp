859fa0c1ecfad71085c0f6206fb4c1cd5a3b9b79
KONDAPALLI SAI TEJA <saitejak2327@gmail.com>
2026-05-25 14:36:52 +0530

    Time: 11 ms (84.14%) | Memory: 87.8 MB (38.30%) - LeetSync

---
+++ 1814-jump-game-vi/jump-game-vi.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n=nums.size();
+        vector<long long>dp(n,0);
+        dp[0]=nums[0];
+        deque<int>dq;
+        dq.push_back(0);
+        for(int i=1;i<n;i++){
+            while(!dq.empty() and dq.front()<i-k) dq.pop_front();
+            dp[i]=nums[i]+dp[dq.front()];
+            while(!dq.empty() && dp[dq.back()]<=dp[i]) dq.pop_back();
+            dq.push_back(i);
+        }
+        return dp[n-1];
+    }
+};
