5439d99deccdad1176bc93156846da645c6ee471
KONDAPALLI SAI TEJA <saitejak2327@gmail.com>
2026-05-25 10:57:48 +0530

    Time: 3194 ms (5.25%) | Memory: 20 MB (81.41%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        if(s[s.size()-1]!='0') return false;
+        vector<bool>dp(s.size(),false);
+        dp[0]=true;
+        int temp=0;
+        for(int i=1;i<s.size();i++){
+            if(i-minJump>=0 and dp[i-minJump]) temp++;
+            if(i-maxJump-1>=0 and dp[i-maxJump-1]) temp--;
+            dp[i]=(temp>0 and s[i]=='0');
+            cout<<dp[i]<<" ";
+        }
+        return dp[s.size()-1];
+    }
+};
