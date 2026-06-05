53c9814fcd2c7443fcfd1f449591ee8b10aa5748
KONDAPALLI SAI TEJA <saitejak2327@gmail.com>
2026-06-05 15:05:55 +0530

    Time: 1964 ms (5.19%) | Memory: 160.2 MB (89.61%) - LeetSync

---
+++ 3140-count-visited-nodes-in-a-directed-graph/count-visited-nodes-in-a-directed-graph.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int dfs(int node,vector<int>&edges,vector<int>&res){
+        if(res[node]) return res[node];
+        return res[node]=dfs(edges[node],edges,res)+1;
+    }
+    vector<int> countVisitedNodes(vector<int>& edges) {
+        int n=edges.size();
+        vector<int>indeg(n),res(n);
+        for(int i=0;i<n;i++){
+            indeg[edges[i]]++;
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++)if(indeg[i]==0) q.push(i);
+        vector<bool>vis(n);
+        while(!q.empty()){
+            int u=q.front();
+            vis[u]=true;
+            q.pop();
+            indeg[edges[u]]--;
+            if(indeg[edges[u]]==0)q.push(edges[u]);
+            // cout << u << " - \n";
+        }
+        
+        for(int i=0;i<n;i++){
+            if(vis[i])continue;
+            int ans=0;
+            int k=i;
+            while(indeg[k]!=0 and !vis[k]){
+                
+                ans++;
+                vis[k]=true;
+                k=edges[k];
+            }
+            k=i;
+            while(res[k]==0){
+                res[k]=ans;
+                k=edges[k];
+            }
+
+        }
+        for(int i=0;i<n;i++) cout<<res[i]<<" ";
+        for(int i=0;i<n;i++){
+            if(res[i]==0){
+                dfs(i,edges,res);
+            }
+        }
+        return res;
+    }
+};
