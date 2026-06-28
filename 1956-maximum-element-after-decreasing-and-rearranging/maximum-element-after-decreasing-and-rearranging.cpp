fd61b9f22d2ee1ea59319823b28b7573b350e7cb
KONDAPALLI SAI TEJA <saitejak2327@gmail.com>
2026-06-28 07:44:05 +0530

    Time: 8 ms (63.84%) | Memory: 55.2 MB (69.21%) - LeetSync

---
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int ans=1;
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]>ans)ans++;
+        }
+        return ans;
+    }
+};
