1284d6c55cdbf476c896f5c6c4a52741f90fc192
KONDAPALLI SAI TEJA <saitejak2327@gmail.com>
2026-08-11 15:28:48 +0530

    Time: 108 ms (6.17%) | Memory: 70.4 MB (5.06%) - LeetSync

---
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    vector<int>primes;
+    void linear_sieve(vector<int>&spf){
+
+        for(int i=2;i<=1e5;i++){
+            if(spf[i]==0){
+                spf[i]=i;
+                primes.push_back(i);
+            }
+            for(int j=0;j<primes.size() and primes[j]<=spf[i] and i*primes[j]<=1e5;j++){
+                spf[i*primes[j]]=primes[j];
+            }
+        }
+    }
+    int distinctPrimeFactors(vector<int>& nums) {
+        vector<int>spf(1e5+1,0);
+        linear_sieve(spf);
+        set<int>st;
+        for(int i=0;i<nums.size();i++){
+            while(nums[i]>1){
+                int p=spf[nums[i]];
+                st.insert(p);
+                while(p==spf[nums[i]]){
+                    nums[i]/=p;
+                }
+            }
+        }
+        return st.size();
+    }
+};
