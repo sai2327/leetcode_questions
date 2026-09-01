567669adbf0ca71a2b7673932dc2e60b7a1dc9bb
KONDAPALLI SAI TEJA <saitejak2327@gmail.com>
2026-09-01 11:03:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (9.33%) - LeetSync

---
+++ 1307-ugly-number-iii/ugly-number-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long lcm(long long x,long long y) {
+        return (x*y)/std::gcd(x, y);
+    }
+    int nthUglyNumber(int n, int a, int b, int c) {
+        long long low=1,high=2e9;
+        long long ab=lcm(a,b),bc=lcm(b,c),ac=lcm(a,c);
+        long long abc=lcm(ab,c),res=0;
+        while(low<=high){
+            long long mid=low+(high-low)/2;
+            long long val=(mid/a)+(mid/b)+(mid/c)+(mid/abc)-(mid/ab)-(mid/bc)-(mid/ac);
+            if(val>=n){
+                res=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return res;
+    }
+};
